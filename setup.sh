cf1980ca8976975ebcb114e2ad12bba94790bfda
zhaoloving <zzb12345@gmail.com>
2022-03-23 21:58:20 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,10 @@
 sudo apt-get install git make
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
+
+gh auth login
+
 git clone https://github.com/zhaoloving/setup.git SETUP && cd SETUP && git remote set-url origin git@github.com:zhaoloving/setup.git && make git
